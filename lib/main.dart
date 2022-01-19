ae0a6635ac1449eba5c087e999f4825a13614a04
Alejandro Bajo <alebajodiaz@gmail.com>
2022-01-19 14:18:09 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Login Page 2', home: Login());
+  }
+}
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/fondo.jpeg'),
+                  fit: BoxFit.cover
+                )
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.black38,
+              )
+            ),
+            Column(
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      Image.asset('pues alguno'),
+                      Text('Algun texto guapo debajo del logo')
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+         )
+      )
+    );
+  }
+}
